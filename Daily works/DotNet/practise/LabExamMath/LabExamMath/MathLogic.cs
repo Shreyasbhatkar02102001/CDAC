7645c4b69c111e490da15e2bf5d93808ec414ec8
Shreyasbhatkar02102001 <130636995+Shreyasbhatkar02102001@users.noreply.github.com>
2025-01-15 23:40:50 +0530

    .

---
+++ Daily works/DotNet/practise/LabExamMath/LabExamMath/MathLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExamMath
+{
+    public class MathLogic
+    {
+        public int Add(int x, int y) => x + y;
+
+        public int Sub(int  x, int y) => x - y;
+
+        public int Mul(int x, int y) => x * y;
+
+        public int Div(int x, int y) => x / y;
+    }
+}
---
+++ Daily works/DotNet/practise/LabExamMath/LabExamMathTest/TestCase.cs
@@ -0,0 +1,50 @@
+﻿using LabExamMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExamMathTest
+{
+    [TestClass]
+    public class TestCase
+    {
+        private MathLogic mathLogic;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            mathLogic = new MathLogic();
+        }
+
+        [TestMethod]
+        public void Addition()
+        {
+            int result = mathLogic.Add(20, 10);
+            Assert.AreEqual(30, result);
+        }
+
+        [TestMethod]
+        public void Subtraction()
+        {
+            int result = mathLogic.Sub(20, 10);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Multiplication()
+        {
+            int result = mathLogic.Mul(20, 10);
+            Assert.AreEqual(200, result);
+        }
+
+        [TestMethod]
+        public void Division()
+        {
+            int result = mathLogic.Div(20, 10);
+            Assert.AreEqual(2, result);
+        }
+
+    }
+}
