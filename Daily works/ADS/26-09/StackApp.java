4fa4fe0399b7d3bbbb0b1dcfb95a088474e9ccde
Shreyasbhatkar02102001 <130636995+Shreyasbhatkar02102001@users.noreply.github.com>
2025-01-18 15:50:44 +0530

    .

--- Daily works/ADS/26-09/StackApp.java
+++ Daily works/ADS/26-09/StackApp.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-class Stack{
+
+class StackApp{
 	
 	private int max;
 	private int top;
 	private int s1[];
 	
-	Stack(int s){
+	StackApp(int s){
 		top = -1;
 		max = s;
 		s1 = new int[max];
@@ -29,13 +30,13 @@ class Stack{
 		}
 	}
 	
-	public void pop(){
+	public int pop(){
 		if (isEmpty()){
 			System.out.println("Undeflow");
 		}
-		else{
-			s1[top--];
-		}
+		
+		return	s1[top--];
+		
 	}
 	
 	public int peek(){
@@ -58,25 +59,27 @@ class Stack{
 	}
 	
 
-}
+//}
 
 
-class StackApp{
+//class StackApp{
+	
 
 	
 	public static void main(String args[]){
 		System.out.print("Enter : ");
 		Scanner sc = new Scanner(System.in);
-		Stack s = new Stack(n);
+		//int n = sc.nextInt();
+		StackApp s = new StackApp(3);
 		
 		s.push(1);
 		s.push(2);
 		s.push(3);
 		
 		System.out.println();
-		s.peek(4);
+		int a = s.peek();
 		
-		System.out.println();
+		System.out.println(a);
 		s.display();
 		
 		System.out.println();
@@ -88,3 +91,86 @@ class StackApp{
 
 
 }
+
+
+/*
+
+class StackApp {
+    
+    private int max;
+    private int top;
+    private int s1[];
+    
+    StackApp(int s) {
+        top = -1;
+        max = s;
+        s1 = new int[max];
+    }
+    
+    public boolean isEmpty() {
+        return top == -1;
+    }
+    
+    public boolean isFull() {
+        return top == (max - 1);
+    }
+    
+    public void push(int x) {
+        if (isFull()) {
+            System.out.println("Overflow");
+        } else {
+            s1[++top] = x;
+        }
+    }
+    
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Underflow");
+            return -1; // Return a default value or throw an exception
+        }
+        return s1[top--];
+    }
+    
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return s1[top];
+    }
+    
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+        } else {
+            for (int i = 0; i <= top; i++) {
+                System.out.println(s1[i]);
+            }
+        }
+    }
+    
+    public static void main(String args[]) {
+        System.out.print("Enter stack size: ");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        StackApp s = new StackApp(size);
+        
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        
+        System.out.println();
+        int a = s.peek();
+        System.out.println("Top element: " + a);
+        
+        System.out.println("Stack elements:");
+        s.display();
+        
+        System.out.println();
+        System.out.println("Popping an element...");
+        s.pop();
+        System.out.println("Stack elements after pop:");
+        s.display();
+    }
+}
+*/
